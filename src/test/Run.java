1dc25b21758924b3d98a9c647f51d9f8b01664e3
zhangzm201801 <zhangzm1117@126.com>
2019-05-02 11:32:53 +0900

    动态添加perimits的个数

--- src/test/Run.java
+++ src/test/Run.java
@@ -1,19 +1,41 @@
 package test;
 
 import service.Service;
+
+import java.util.concurrent.Semaphore;
+
 import extthread.ThreadA;
 
 public class Run {
 
 	public static void main(String[] args) throws InterruptedException {
-		Service service = new Service();
-
-		ThreadA[] a = new ThreadA[10];
-		for (int i = 0; i < 10; i++) {
-			a[i] = new ThreadA(service);
-			a[i].start();
-		}
-
+		
+		Semaphore semaphore = new Semaphore(5);
+		semaphore.acquire();
+		semaphore.acquire();
+		semaphore.acquire();
+		semaphore.acquire();
+		semaphore.acquire();
+		System.out.println(semaphore.availablePermits());
+		
+		semaphore.release();
+		semaphore.release();
+		semaphore.release();
+		semaphore.release();
+		semaphore.release();
+		semaphore.release();
+		System.out.println(semaphore.availablePermits());
+		
+		semaphore.release(4);
+		System.out.println(semaphore.availablePermits());
+//		Service service = new Service();
+//
+//		ThreadA[] a = new ThreadA[10];
+//		for (int i = 0; i < 10; i++) {
+//			a[i] = new ThreadA(service);
+//			a[i].start();
+//		}
+//
 	}
 
 }
